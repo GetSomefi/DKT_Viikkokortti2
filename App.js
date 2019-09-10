ae2bf6fde037b10ab4979e9cf2457d4b647fcbfa
Peter Virtanen <peyte.com@gmail.com>
2019-09-10 15:20:21 +0300

    kysymykset paikallaan

--- App.js
+++ App.js
@@ -18,65 +18,6 @@ import Lister from './components/DataFragment.js';
 
 
 var options = [
-
-  //taidot
-  {
-    groupId:3,
-    groupSafename:"Taidot",
-    items:[
-      {
-        hasInner:true,
-        innerGroupId:0,
-        groupSafename:"Tietoisuustaidot",
-        innerItems:[
-          {
-            id: 0, 
-            type: "selectone",
-            questionSafename:"Viisasmieli",
-            extraName:"",
-          },
-          {
-            id: 1, 
-            type: "selectone",
-            questionSafename:"Havainnointi (huomio mitä tapahtui)",
-            extraName:"Mitä -taidot",
-          },
-          {
-            id: 2, 
-            type: "selectone",
-            questionSafename:"Kuvailu (anna havaintoihin sanat)",
-            extraName:"",
-          },
-          {
-            id: 3, 
-            type: "selectone",
-            questionSafename:"Osallistuminen (heittäydy kokemukseesi)",
-            extraName:"",
-          },
-          {
-            id: 4, 
-            type: "selectone",
-            questionSafename:"Tuomitsemattomuus",
-            extraName:"Miten -taidot",
-          },
-        ]
-      },
-      {
-        hasInner:true,
-        innerGroupId:1,
-        groupSafename:"Ahdingon sietämisen taidot",
-        innerItems:[
-          {
-            id: 0, 
-            type: "selectone",
-            questionSafename:"STOP",
-            extraName:"",
-          },
-        ]
-      }
-    ]
-  },
-
   //käyttäytyminen
   {
     groupId:1,
@@ -121,16 +62,71 @@ var options = [
         ] 
       },
       {
-        id:2,
-        type:"selectone",
-        questionSafename:"Alkoholi",
-        extraName:"",
-      },
-      {
-        id:3,
-        type:"selectone",
-        questionSafename:"Huumeet",
-        extraName:"",
+        hasInner:true,
+        innerGroupId:0,
+        groupSafename:"",
+        innerItems:[
+          {
+            id:2,
+            type:"selectone",
+            questionSafename:"Alkoholi",
+            extraName:"",
+          },
+          {
+            id:3,
+            type:"selectone",
+            questionSafename:"Huumeet",
+            extraName:"",
+          },
+          {
+            id:4,
+            type:"selectone",
+            questionSafename:"Lääkehoito",
+            extraName:"",
+          },
+          {
+            id:5,
+            type:"selectone",
+            questionSafename:"Poissaolo sovitusta menosta",
+            extraName:"",
+          },
+          {
+            id:6,
+            type:"selectone",
+            questionSafename:"Maltin menetys",
+            extraName:"",
+          },
+          {
+            id:7,
+            type:"selectone",
+            questionSafename:"Ahminta / syömättömyys",
+            extraName:"",
+          },
+          {
+            id:8,
+            type:"selectone",
+            questionSafename:"Oksentaminen",
+            extraName:"",
+          },
+          {
+            id:9,
+            type:"selectone",
+            questionSafename:"Riskiseksi",
+            extraName:"",
+          },
+          {
+            id:10,
+            type:"selectone",
+            questionSafename:"Valehtelu",
+            extraName:"",
+          },
+          {
+            id:11,
+            type:"selectone",
+            questionSafename:"Vapaa teksti tähän",
+            extraName:"",
+          }
+        ]
       }
     ]
   },
@@ -142,15 +138,247 @@ var options = [
     items:[
       {
         id: 0, 
-        type: "scale",
-        questionSafename:"Viha",
-        extraName:"",
-        list:[1,2,3,4,5]
-      }
+        type: "selection",
+        questionSafename:"",
+        questions:[
+          {
+            id: 0,
+            safename:"Viha",
+            extraName:"",
+            list:[1,2,3,4,5]
+          },
+          {
+            id: 1,
+            safename:"Pelko",
+            extraName:"",
+            list:[1,2,3,4,5]
+          },
+          {
+            id: 2,
+            safename:"Ahdistus",
+            extraName:"",
+            list:[1,2,3,4,5]
+          },
+          {
+            id: 3,
+            safename:"Ilo",
+            extraName:"",
+            list:[1,2,3,4,5]
+          },
+          {
+            id: 4,
+            safename:"Suru",
+            extraName:"",
+            list:[1,2,3,4,5]
+          },
+          {
+            id: 5,
+            safename:"Häpeä",
+            extraName:"",
+            list:[1,2,3,4,5]
+          }
+        ]     
+      },
     ]
   },
 
-
+  //taidot
+  {
+    groupId:3,
+    groupSafename:"Taidot",
+    items:[
+      {
+        hasInner:true,
+        innerGroupId:0,
+        groupSafename:"Tietoisuustaidot",
+        innerItems:[
+          {
+            id: 0, 
+            type: "selectone",
+            questionSafename:"Viisasmieli",
+            extraName:"",
+          },
+          {
+            id: 1, 
+            type: "selectone",
+            questionSafename:"Havainnointi (huomio mitä tapahtui)",
+            extraName:"Mitä -taidot",
+          },
+          {
+            id: 2, 
+            type: "selectone",
+            questionSafename:"Kuvailu (anna havaintoihin sanat)",
+            extraName:"",
+          },
+          {
+            id: 3, 
+            type: "selectone",
+            questionSafename:"Osallistuminen (heittäydy kokemukseesi)",
+            extraName:"",
+          },
+          {
+            id: 4, 
+            type: "selectone",
+            questionSafename:"Tuomitsemattomuus",
+            extraName:"Miten -taidot",
+          },
+          {
+            id: 5, 
+            type: "selectone",
+            questionSafename:"Asia kerrallaan",
+            extraName:"",
+          },
+          {
+            id: 6, 
+            type: "selectone",
+            questionSafename:"Tee sitä mikä toimii",
+            extraName:"(tehokkuus)",
+          }
+        ]
+      },
+      {
+        hasInner:true,
+        innerGroupId:1,
+        groupSafename:"Ahdingon sietämisen taidot",
+        innerItems:[
+          {
+            id: 0, 
+            type: "selectone",
+            questionSafename:"STOP",
+            extraName:"",
+          },
+          {
+            id: 1, 
+            type: "selectone",
+            questionSafename:"Plussat ja miinukset",
+            extraName:"",
+          },
+          {
+            id: 2, 
+            type: "selectone",
+            questionSafename:"TIPP",
+            extraName:"",
+          },
+          {
+            id: 3, 
+            type: "selectone",
+            questionSafename:"Huomion siirtäminen muualle",
+            extraName:"",
+          },
+          {
+            id: 4, 
+            type: "selectone",
+            questionSafename:"Itsensä rauhoittaminen",
+            extraName:"",
+          },
+          {
+            id: 5, 
+            type: "selectone",
+            questionSafename:"Hetken parantaminen",
+            extraName:"",
+          },
+          {
+            id: 6, 
+            type: "selectone",
+            questionSafename:"Radikaali hyväksyminen",
+            extraName:"",
+          },
+          {
+            id: 7, 
+            type: "selectone",
+            questionSafename:"Riippuvuuksien hallinta",
+            extraName:"",
+          }
+        ]
+      },
+      {
+        hasInner:true,
+        innerGroupId:2,
+        groupSafename:"Tunteiden säätelyn taidot",
+        innerItems:[
+          {
+            id: 0, 
+            type: "selectone",
+            questionSafename:"Tunteiden ymmärtäminen ja kuvaaminen",
+            extraName:"",
+          },
+          {
+            id: 1, 
+            type: "selectone",
+            questionSafename:"Toimiminen toisin kuin tunne yllyttää",
+            extraName:"",
+          },
+          {
+            id: 2, 
+            type: "selectone",
+            questionSafename:"Lyhyen ja pitkän aikavälin tavoitteet",
+            extraName:"",
+          },
+          {
+            id: 3, 
+            type: "selectone",
+            questionSafename:"Hallinnan tunteen kehittäminen",
+            extraName:"",
+          },
+          {
+            id: 4, 
+            type: "selectone",
+            questionSafename:"PLEASE",
+            extraName:"Terveydestä huolehtiminen",
+          }
+        ]
+      },
+      {
+        hasInner:true,
+        innerGroupId:3,
+        groupSafename:"Vuorovaikutustaidot",
+        innerItems:[
+          {
+            id: 0, 
+            type: "selectone",
+            questionSafename:"DEAR MAN",
+            extraName:"Saadaksesi mitä haluat",
+          },
+          {
+            id: 1, 
+            type: "selectone",
+            questionSafename:"GIVE",
+            extraName:"Suhteen muuttaminen paremmaksi",
+          },
+          {
+            id: 2, 
+            type: "selectone",
+            questionSafename:"FAST",
+            extraName:"Itsekunnioituksen säilyttäminen",
+          },
+          {
+            id: 3, 
+            type: "selectone",
+            questionSafename:"Uusien ihmissuhteiden luominen ja haitallisten suhteiden päättäminen",
+            extraName:"",
+          },
+          {
+            id: 4, 
+            type: "selectone",
+            questionSafename:"Dialektiikka",
+            extraName:"",
+          },
+          {
+            id: 5, 
+            type: "selectone",
+            questionSafename:"Validointi",
+            extraName:"",
+          },
+          {
+            id: 6, 
+            type: "selectone",
+            questionSafename:"Käytöksen muuttamisen keinot",
+            extraName:"",
+          }
+        ]
+      }
+    ]
+  },
 
 ];
 
--- components/DataFragment.js
+++ components/DataFragment.js
@@ -22,12 +22,14 @@ class Lister extends Component {
     //let p = this.props.options;
     //console.log(p);
     this.state = {
+      showDebug:false, //shows keys if those are set inside elements
+
       tc: "white",
       bg: "blue",
       props:this.props.options,
-      activeColor:"#8bc586",
+      activeColor:"#8bc586", //green
       inactiveColor:"white",
-      activeColor2:"#f7f7f7",
+      activeColor2:"#f7f7f7", //light gray
       inactiveColor2:"white",
 
       activeIconOpacity:1,
@@ -48,16 +50,14 @@ class Lister extends Component {
   }
 
   createEl = () => {
-    console.log('colo', this.state.bg);
-    let colo = this.state.bg;
     //let listItems = this.state.props.map(function(item, i){ <- Tämä tapa ei toimi koska this unohtuu (eri scope)
     let listItems = this.state.props.map((item, i) => {
       //console.log('das',this.state.bg); 
       let ret = [];
-      console.log('item',item); 
+      //console.log('item',item); 
       ret.push(
         <View key="groupId{item.groupId}">
-          <Text style={{fontSize: 20}}>{item.groupSafename}</Text>
+          <Text style={styles.header1}>{item.groupSafename}</Text>
         </View> 
       ); 
 
@@ -89,42 +89,116 @@ class Lister extends Component {
                     </View>
                   </View>
                   <View style={styles.selectableContent}>
-                    <Text>#{key}# {innerItems.questionSafename}</Text>
+                    <Text>
+                      { this.state.showDebug ? (<Text style={styles.debugTexts}>#{key}#</Text>) : null }
+                      {innerItems.questionSafename}
+                      {innerItems.extraName != "" ? "\n" : null}
+                      {innerItems.extraName}
+                    </Text>
                   </View>
                 </View>
               </TouchableHighlight>
             );
           });
-        }else{
-          var key = "questionName" + item.groupId + "_" + itemInner.id;
-          ret.push(
-            <View key={key}>
-              <Text>#{key}# {itemInner.questionSafename}</Text>
-            </View>
-          );
+        }
+        else{
+          var key = "questionName" + item.groupId + "_" + itemInner.id;          
           if( itemInner.type == "selection" ){
+            
+            if(itemInner.questionSafename){
+	            ret.push(
+	              <View key={key}>
+
+	                <Text style={styles.header2}>
+	                  { this.state.showDebug ? (<Text style={styles.debugTexts}>#{key}#</Text>) : null }
+	                  {itemInner.questionSafename}
+	                </Text>
+	              </View>
+	            );
+	        }
+
             itemInner.questions.map( (innerItems, i3) => { 
               var key = "theQuestionName" + innerItems.id + "_" + itemInner.id;
-              ret.push(
-                <View key={key}>
-                  <Text>#{key}# {innerItems.safename}</Text>  
-                </View>
-                
-              );
+              
+              if(innerItems.safename){
+	              ret.push(
+	                <View key={key}>
+	                  <Text style={styles.header3}>
+	                    { this.state.showDebug ? (<Text style={styles.debugTexts}>#{key}#</Text>) : null }
+	                    {innerItems.safename}
+	                  </Text>  
+	                </View>
+	              );
+			  }          
+
+              let ret2 = [];
               innerItems.list.map( (selList, i4) => {
-                var key = "selList" + i4 + "_" + i3 + "_" + i2;
-                ret.push(  
-                  <TouchableHighlight key={key}>
-                    <Text>#{key}# {selList}</Text>  
+                var key = "selList" + item.groupId + "_" + i4 + "_" + i3 + "_" + i2;
+                ret2.push(  
+                  <TouchableHighlight
+                    onPress={ (evt) => this.onPress(key,evt) } 
+                    key={key}>
+                      <View style={{...styles.selectableChoice,
+                      backgroundColor: this.state[key +'-active'] ? this.state.activeColor : this.state.inactiveColor,
+                      }}>
+                        <Text>
+                          { this.state.showDebug ? (<Text style={styles.debugTexts}>#{key}#</Text>) : null }
+                          {selList}
+                        </Text> 
+                      </View> 
                   </TouchableHighlight>
                 );
               });
+
+              var key = "theQuestionChoices" + innerItems.id + "_" + itemInner.id;
+              ret.push(
+                <View key={key} style={styles.selectableChoiceParent}>
+                  {ret2} 
+                </View>
+                
+              );
 
             });
           }        
         }
       });
 
+
+/*
+          if(item.type = "selectone"){
+            console.log('type', item); 
+            ret.push(
+              <TouchableHighlight
+                onPress={ (evt) => this.onPress(key,evt) } 
+                key={key}>
+                <View style={{...styles.selectableParent,
+                backgroundColor: this.state[key +'-active'] ? this.state.activeColor : this.state.inactiveColor,
+                }}>
+                  <View style={{margin:15}}>
+                    <View style={{...styles.selectableCheckbox,
+                    backgroundColor: this.state[key +'-active'] ? this.state.activeColor2 : this.state.inactiveColor2,
+                    }}>
+                      <Image
+                        style={{
+                          width: 32, 
+                          height: 32,
+                          opacity: this.state[key +'-active'] ? this.state.activeIconOpacity : 0,
+                        }}
+                        source={require('../assets/icons/times_320_320.png')}
+                      />
+                    </View>
+                  </View>
+                  <View style={styles.selectableContent}>
+                    <Text>
+                      { this.state.showDebug ? (<Text style={styles.debugTexts}>#{key}#</Text>) : null }
+                      {item.questionSafename}
+                    </Text>
+                  </View>
+                </View>
+              </TouchableHighlight>
+            );
+          }
+*/
 
       var key = "masterView" + item.groupId;
       return(
@@ -146,32 +220,67 @@ class Lister extends Component {
 
 let perfectCheckboxSize = 50;
 let padding = 15;
+let borderRadius = 5;
 
 let perfectChecbox = perfectCheckboxSize;
 const styles = StyleSheet.create({
+  debugTexts:{
+    backgroundColor:"red"
+  },
+
+  header1:{
+    fontSize: 28,
+    padding:padding,
+  },
+  header2:{
+    fontSize: 24,
+    padding:padding,
+  },
+  header3:{
+    fontSize: 20,
+    padding:padding,
+  },
+
   selectableParent: {
     flex:1,
     flexDirection:'row',
     backgroundColor: '#FFF',
     borderColor: '#d6d7da',
-    marginTop:5,
-    marginBottom:5,
     justifyContent:"center",
     alignItems:"center",
+    margin:padding,
+    borderRadius:borderRadius,
   },
   selectableCheckbox: {
     height:perfectChecbox,
     width:perfectChecbox,
     alignItems:"center",
     justifyContent:"center",
-    borderRadius:5,
+    borderRadius:borderRadius,
     borderWidth:1,
-    borderColor:'#d6d7da'
+    borderColor:'#d6d7da',
+    marginLeft:(padding*2),
   },
   selectableContent: {
     width:width-perfectChecbox - (padding*2),
     //backgroundColor:"#FFF",
     padding:padding,
+    paddingLeft:0,
+  },
+  selectableChoiceParent: {
+    flex:1,
+    flexDirection:'row',
+    padding:padding
+  },
+  selectableChoice: {
+    height:perfectChecbox,
+    width:perfectChecbox,
+    alignItems:"center",
+    justifyContent:"center",
+    borderRadius:borderRadius,
+    borderWidth:1,
+    borderColor:'#d6d7da',
+    marginRight:padding
   },
 });
 
