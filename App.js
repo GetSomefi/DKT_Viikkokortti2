f9fdeab4e3f9bf826bd67067d75a1182247fbfbe
Peter Virtanen <peyte.com@gmail.com>
2019-09-18 15:23:54 +0300

    tallennus toimii

--- App.js
+++ App.js
@@ -20,25 +20,27 @@ import Lister from './components/DataFragment.js';
 var options = [
   //käyttäytyminen
   {
-    groupId:1,
+    id:0,
     groupSafename:"Käyttäytyminen",
     items:[
       {
         id: 0, 
         type: "selection",
         questionSafename:"Itsensä vahingoittaminen",
-        questions:[
+        items:[
           {
             id: 0,
             safename:"Ajatukset",
             extraName:"",
-            list:[1,2,3,4,5]
+            list:[1,2,3,4,5],
+            selectedValue:""
           },
           {
             id: 1,
             safename:"Teot",
             extraName:"",
-            list:[1,2,3,4,5]
+            list:[1,2,3,4,5],
+            selectedValue:""
           }
         ]     
       },
@@ -46,79 +48,90 @@ var options = [
         id: 1, 
         type: "selection",
         questionSafename:"Itsemurha aikeet",
-        questions:[
+        items:[
           {
             id: 0,
             safename:"Ajatukset",
             extraName:"",
-            list:[1,2,3,4,5]
+            list:[1,2,3,4,5],
+            selectedValue:""
           },
           {
             id: 1,
             safename:"Teot",
             extraName:"",
-            list:[1,2,3,4,5]
+            list:[1,2,3,4,5],
+            selectedValue:""
           }
         ] 
       },
       {
         hasInner:true,
-        innerGroupId:0,
+        id:0,
         groupSafename:"",
-        innerItems:[
+        items:[
           {
             id:2,
             type:"selectone",
             questionSafename:"Alkoholi",
             extraName:"",
+            selectedValue:"qqq"
           },
           {
             id:3,
             type:"selectone",
             questionSafename:"Huumeet",
             extraName:"",
+            selectedValue:""
           },
           {
             id:4,
             type:"selectone",
             questionSafename:"Lääkehoito",
             extraName:"",
+            selectedValue:""
           },
           {
             id:5,
             type:"selectone",
             questionSafename:"Poissaolo sovitusta menosta",
             extraName:"",
+            selectedValue:""
           },
           {
             id:6,
             type:"selectone",
             questionSafename:"Maltin menetys",
             extraName:"",
+            selectedValue:""
           },
           {
             id:7,
             type:"selectone",
             questionSafename:"Ahminta / syömättömyys",
             extraName:"",
+            selectedValue:""
           },
           {
             id:8,
             type:"selectone",
             questionSafename:"Oksentaminen",
             extraName:"",
+            selectedValue:""
           },
           {
             id:9,
             type:"selectone",
             questionSafename:"Riskiseksi",
             extraName:"",
+            selectedValue:""
           },
           {
             id:10,
             type:"selectone",
             questionSafename:"Valehtelu",
             extraName:"",
+            selectedValue:""
           },
           {
             id:11,
@@ -133,49 +146,55 @@ var options = [
 
   //tunteet
   {
-    groupId:2,
+    id:1,
     groupSafename:"Tunteet",
     items:[
       {
         id: 0, 
         type: "selection",
         questionSafename:"",
-        questions:[
+        items:[
           {
             id: 0,
             safename:"Viha",
             extraName:"",
-            list:[1,2,3,4,5]
+            list:[1,2,3,4,5],
+            selectedValue:""
           },
           {
             id: 1,
             safename:"Pelko",
             extraName:"",
-            list:[1,2,3,4,5]
+            list:[1,2,3,4,5],
+            selectedValue:""
           },
           {
             id: 2,
             safename:"Ahdistus",
             extraName:"",
-            list:[1,2,3,4,5]
+            list:[1,2,3,4,5],
+            selectedValue:""
           },
           {
             id: 3,
             safename:"Ilo",
             extraName:"",
-            list:[1,2,3,4,5]
+            list:[1,2,3,4,5],
+            selectedValue:""
           },
           {
             id: 4,
             safename:"Suru",
             extraName:"",
-            list:[1,2,3,4,5]
+            list:[1,2,3,4,5],
+            selectedValue:""
           },
           {
             id: 5,
             safename:"Häpeä",
             extraName:"",
-            list:[1,2,3,4,5]
+            list:[1,2,3,4,5],
+            selectedValue:""
           }
         ]     
       },
@@ -184,191 +203,217 @@ var options = [
 
   //taidot
   {
-    groupId:3,
+    id:2,
     groupSafename:"Taidot",
     items:[
       {
         hasInner:true,
-        innerGroupId:0,
+        id:0,
         groupSafename:"Tietoisuustaidot",
-        innerItems:[
+        items:[
           {
             id: 0, 
             type: "selectone",
             questionSafename:"Viisasmieli",
             extraName:"",
+            selectedValue:""
           },
           {
             id: 1, 
             type: "selectone",
             questionSafename:"Havainnointi (huomio mitä tapahtui)",
             extraName:"Mitä -taidot",
+            selectedValue:""
           },
           {
             id: 2, 
             type: "selectone",
             questionSafename:"Kuvailu (anna havaintoihin sanat)",
             extraName:"",
+            selectedValue:""
           },
           {
             id: 3, 
             type: "selectone",
             questionSafename:"Osallistuminen (heittäydy kokemukseesi)",
             extraName:"",
+            selectedValue:""
           },
           {
             id: 4, 
             type: "selectone",
             questionSafename:"Tuomitsemattomuus",
             extraName:"Miten -taidot",
+            selectedValue:""
           },
           {
             id: 5, 
             type: "selectone",
             questionSafename:"Asia kerrallaan",
             extraName:"",
+            selectedValue:""
           },
 
         ]
       },
       {
         hasInner:true,
-        innerGroupId:1,
+        id:1,
         groupSafename:"Ahdingon sietämisen taidot",
-        innerItems:[
+        items:[
           {
             id: 0, 
             type: "selectone",
             questionSafename:"STOP",
             extraName:"",
+            selectedValue:""
           },
           {
             id: 1, 
             type: "selectone",
             questionSafename:"Plussat ja miinukset",
             extraName:"",
+            selectedValue:""
           },
           {
             id: 2, 
             type: "selectone",
             questionSafename:"TIPP",
             extraName:"",
+            selectedValue:""
           },
           {
             id: 3, 
             type: "selectone",
             questionSafename:"Huomion siirtäminen muualle",
             extraName:"",
+            selectedValue:""
           },
           {
             id: 4, 
             type: "selectone",
             questionSafename:"Itsensä rauhoittaminen",
             extraName:"",
+            selectedValue:""
           },
           {
             id: 5, 
             type: "selectone",
             questionSafename:"Hetken parantaminen",
             extraName:"",
+            selectedValue:""
           },
           {
             id: 6, 
             type: "selectone",
             questionSafename:"Radikaali hyväksyminen",
             extraName:"",
+            selectedValue:""
           },
           {
             id: 7, 
             type: "selectone",
             questionSafename:"Riippuvuuksien hallinta",
             extraName:"",
+            selectedValue:""
           }
         ]
       },
       {
         hasInner:true,
-        innerGroupId:2,
+        id:2,
         groupSafename:"Tunteiden säätelyn taidot",
-        innerItems:[
+        items:[
           {
             id: 0, 
             type: "selectone",
             questionSafename:"Tunteiden ymmärtäminen ja kuvaaminen",
             extraName:"",
+            selectedValue:""
           },
           {
             id: 1, 
             type: "selectone",
             questionSafename:"Toimiminen toisin kuin tunne yllyttää",
             extraName:"",
+            selectedValue:""
           },
           {
             id: 2, 
             type: "selectone",
             questionSafename:"Lyhyen ja pitkän aikavälin tavoitteet",
             extraName:"",
+            selectedValue:""
           },
           {
             id: 3, 
             type: "selectone",
             questionSafename:"Hallinnan tunteen kehittäminen",
             extraName:"",
+            selectedValue:""
           },
           {
             id: 4, 
             type: "selectone",
             questionSafename:"PLEASE",
             extraName:"Terveydestä huolehtiminen",
+            selectedValue:""
           }
         ]
       },
       {
         hasInner:true,
-        innerGroupId:3,
+        id:3,
         groupSafename:"Vuorovaikutustaidot",
-        innerItems:[
+        items:[
           {
             id: 0, 
             type: "selectone",
             questionSafename:"DEAR MAN",
             extraName:"Saadaksesi mitä haluat",
+            selectedValue:""
           },
           {
             id: 1, 
             type: "selectone",
             questionSafename:"GIVE",
             extraName:"Suhteen muuttaminen paremmaksi",
+            selectedValue:""
           },
           {
             id: 2, 
             type: "selectone",
             questionSafename:"FAST",
             extraName:"Itsekunnioituksen säilyttäminen",
+            selectedValue:""
           },
           {
             id: 3, 
             type: "selectone",
             questionSafename:"Uusien ihmissuhteiden luominen ja haitallisten suhteiden päättäminen",
             extraName:"",
+            selectedValue:""
           },
           {
             id: 4, 
             type: "selectone",
             questionSafename:"Dialektiikka",
             extraName:"",
+            selectedValue:""
           },
           {
             id: 5, 
             type: "selectone",
             questionSafename:"Validointi",
             extraName:"",
+            selectedValue:""
           },
           {
             id: 6, 
             type: "selectone",
             questionSafename:"Käytöksen muuttamisen keinot",
             extraName:"",
+            selectedValue:""
           }
         ]
       }
--- components/DataFragment.js
+++ components/DataFragment.js
@@ -2,6 +2,8 @@
 
 import React, { Component } from 'react';
 
+import AsyncStorage from '@react-native-community/async-storage'; //https://github.com/react-native-community/async-storage/blob/LEGACY/docs/API.md#getItem
+
 import {
   View,
   Text, 
@@ -10,76 +12,136 @@ import {
   Dimensions,
   Image,
   TextInput,
+  UseEffect
 } from 'react-native';
 
 var width = Dimensions.get('window').width; //full width
 var height = Dimensions.get('window').height; //full height
 
 class Lister extends Component {
+	constructor(props) {
+		super(props);
+		//let p = this.props.options;
+		//console.log(p);
+		this.state = {
+		  showDebug:false, //shows keys if those are set inside elements
+		  //showDebug:true, //shows keys if those are set inside elements
+
+		  tc: "white",
+		  bg: "blue",
+		  propsOptions:this.props.options,
+		  activeColor:"#8bc586", //green
+		  inactiveColor:"white",
+		  activeColor2:"#f7f7f7", //lighter gray
+		  inactiveColor2:"white",
+		  activeColor3:"#f1f1f1", //light gray
+		  inactiveColor3:"#FFF",
+
+		  activeIconOpacity:1,
+		};
+	}
+
+	choiceSelected = (key,evt,el) => {
+		console.log("-- key to store",key);
+
+		let selected = true;
+		if( this.state[key +'-active'] ){
+		  selected = false;
+		}
+		console.log('selected',selected); 
+		this.setState({ 
+		  [key +'-active']: selected 
+		});
+
+		console.log('state', this.state);
+
+		console.log('el', el);
+		var d = new Date();
+		let date = d.getDate() + "_" + d.getMonth() + "_" + d.getYear();
+		this.storeData(el,date);
+		//el.questionSafename = "sad";
+		//return el;
+
+		this.setState({
+			[key +'-selectedValue']:"valittu"
+		});
+		console.log(this.state);
+	}
+
+	t = () => {
+		console.log('test'); 
+	}
+
+	debugTexts = () => {
+		console.log('debug texts ', this.state.showDebug); 
+		this.setState({ 
+		  showDebug: !this.state.showDebug
+		});
+	}
+
+	storeData = async (data,key) => {
+		console.log('yrittää'); 
+		try {
+			await AsyncStorage.setItem(key, JSON.stringify(data));
+			
+		} catch (error) {
+			// Error saving data
+			console.log('Save error',error); 
+		}
+		console.log('Key:', key);
+		console.log('Data:', data);
+	};
+
+	retrieveData = async (key) => {
+	  try {
+	    const value = await AsyncStorage.getItem(key);
+	    if (value !== null) {
+	      // We have data!!
+	      console.log(value);
+	    }
+	  } catch (error) {
+	    // Error retrieving data
+	    console.log('Retrieve error',error); 
+	  }
+	};
+
+	createEl = () => {
+		//this.state.propsOptions[i]
+		let copyPropsOptions = [...this.state.propsOptions];
+
+		let ret = [];
+		let listItems = copyPropsOptions.map((item, i) => {
+		  //console.log('das',this.state.bg);  
+		  //console.log('item',item); 
+		  var key = "id|" + item.id;
+		  ret.push(	  	
+		    <View key={key}>
+				{ this.state.showDebug ? (<Text style={styles.debugTexts}>#{key}#</Text>) : null }
+		      	<Text style={styles.header1}>{item.groupSafename}</Text>
+		    </View> 
+		  ); 
 
 
-  constructor(props) {
-    super(props);
-    //let p = this.props.options;
-    //console.log(p);
-    this.state = {
-      showDebug:false, //shows keys if those are set inside elements
+		  //this.state.propsOptions[i][i2]
+		  return item.items.map((itemInner, i2) => {
+		    //console.log('content ', itemInner);
+		    if(itemInner.hasInner){
 
-      tc: "white",
-      bg: "blue",
-      props:this.props.options,
-      activeColor:"#8bc586", //green
-      inactiveColor:"white",
-      activeColor2:"#f7f7f7", //lighter gray
-      inactiveColor2:"white",
-      activeColor3:"#f1f1f1", //light gray
-      inactiveColor3:"#FFF",
-
-      activeIconOpacity:1,
-    };
-  }
-
-  onPress = (key,evt) => {
-    console.log('item',key);
-
-    let selected = true;
-    if( this.state[key +'-active'] ){
-      selected = false;
-    }
-    console.log('selected',selected); 
-    this.setState({ 
-      [key +'-active']: selected 
-    });
-  }
-
-  createEl = () => {
-    //let listItems = this.state.props.map(function(item, i){ <- Tämä tapa ei toimi koska this unohtuu (eri scope)
-    let listItems = this.state.props.map((item, i) => {
-      //console.log('das',this.state.bg); 
-      let ret = [];
-      //console.log('item',item); 
-      ret.push(
-        <View key="groupId{item.groupId}">
-          <Text style={styles.header1}>{item.groupSafename}</Text>
-        </View> 
-      ); 
-
-      item.items.map((itemInner, i2) => {
-        //console.log('content ', itemInner);
-        if(itemInner.hasInner){
-          itemInner.innerItems.map( (innerItems, i3) => {
-            var key = "questionNameInner" + itemInner.innerGroupId + "_" + innerItems.id;
-	           if(innerItems.type == "freetext"){
+		      //this.state.propsOptions[i][i2][i3]
+		      return itemInner.items.map( (innerItems, i3) => {
+		        var key = "questionName|" + item.id + "_" + itemInner.id + "_" + innerItems.id;
+		        let keyArray = [item.id, itemInner.id, innerItems.id];
+				if(innerItems.type == "freetext"){ 
 				ret.push(
 				<TouchableHighlight 
 					key={key+"_freetext"}
 					onPress={() => this.setState({ isEditing: true })}
-	            >
-		            <View style={{...styles.freeTextParent,
-	                	backgroundColor: this.state.isEditing ? this.state.activeColor3 : this.state.inactiveColor3
-	            	}}>
+				>
+				    <View style={{...styles.freeTextParent,
+				    	backgroundColor: this.state.isEditing ? this.state.activeColor3 : this.state.inactiveColor3
+					}}>
 					  <Text style={styles.labelHeader}>
-	                	{ this.state.showDebug ? (<Text style={styles.debugTexts}>#{key}#</Text>) : null }
+				    	{ this.state.showDebug ? (<Text style={styles.debugTexts}>#{key}#</Text>) : null }
 						{innerItems.questionSafename}
 					  </Text>
 
@@ -102,122 +164,159 @@ class Lister extends Component {
 					</View>
 				</TouchableHighlight>
 				)
-	           }  
-	           else{
-	            ret.push(
-	              <TouchableHighlight
-	                onPress={ (evt) => this.onPress(key,evt) } 
-	                key={key}>
-	                <View style={{...styles.selectableParent,
-	                backgroundColor: this.state[key +'-active'] ? this.state.activeColor : this.state.inactiveColor,
-	                }}>
-	                  <View style={{margin:15}}>
-	                    <View style={{...styles.selectableCheckbox,
-	                    backgroundColor: this.state[key +'-active'] ? this.state.activeColor2 : this.state.inactiveColor2,
-	                    }}>
-	                      <Image
-	                        style={{
-	                          width: 32, 
-	                          height: 32,
-	                          opacity: this.state[key +'-active'] ? this.state.activeIconOpacity : 0,
-	                        }}
-	                        source={require('../assets/icons/times_320_320.png')}
-	                      />
-	                    </View>
-	                  </View>
-	                  <View style={styles.selectableContent}>
-	                    <Text>
-	                      { this.state.showDebug ? (<Text style={styles.debugTexts}>#{key}#</Text>) : null }
-	                      {innerItems.questionSafename}
-	                      {innerItems.extraName != "" ? "\n" : null}
-	                      {innerItems.extraName}
-	                    </Text>
-	                  </View>
-	                </View>
-	              </TouchableHighlight>
-	            );
-	          }
-          });
-        }
-        else{
-          var key = "questionName" + item.groupId + "_" + itemInner.id;          
-          if( itemInner.type == "selection" ){
-            
-            if(itemInner.questionSafename){
-	            ret.push(
-	              <View key={key}>
+				}
+				//this.state.propsOptions[i][i2][i3]
+				else{
+				
 
-	                <Text style={styles.header2}>
-	                  { this.state.showDebug ? (<Text style={styles.debugTexts}>#{key}#</Text>) : null }
-	                  {itemInner.questionSafename}
-	                </Text>
-	              </View>
-	            );
-	        }
+				ret.push(
+				  <TouchableHighlight
+				    onPress={ 
+				    	(evt) => {
+				    		innerItems.selectedValue = "true";	
+				    		this.choiceSelected(key,evt,listItems);	
+				    	}
+				    	
+				    	//innerItems.seletedValue = true
+				    } 
+				    key={key}>
+				    <View style={{...styles.selectableParent,
+				    backgroundColor: this.state[key +'-active'] ? this.state.activeColor : this.state.inactiveColor,
+				    }}>
+				      <View style={{margin:15}}>
+				        <View style={{...styles.selectableCheckbox,
+				        backgroundColor: this.state[key +'-active'] ? this.state.activeColor2 : this.state.inactiveColor2,
+				        }}>
+				          <Image
+				            style={{
+				              width: 32, 
+				              height: 32,
+				              opacity: this.state[key +'-active'] ? this.state.activeIconOpacity : 0,
+				            }}
+				            source={require('../assets/icons/times_320_320.png')}
+				          />
+				        </View>
+				      </View>
+				      <View style={styles.selectableContent}>
+				        <Text>
+				          { this.state.showDebug ? (<Text style={styles.debugTexts}>#{key}#</Text>) : null }
+				          {innerItems.questionSafename}
+				          {innerItems.extraName != "" ? "\n" : null}
+				          {innerItems.extraName}
+				          # {innerItems.selectedValue}
+				        </Text>
+				      </View>
+				    </View>
+				  </TouchableHighlight>
+				);
+				}
+				return innerItems;
+		      });
+		    }
+		    else{
+		      var key = "innerGroupName" + item.id + "_" + itemInner.id;          
+		      if( itemInner.type == "selection" ){
+		        
+		        if(itemInner.questionSafename){
+		            ret.push(
+		              <View key={key}>
 
-            itemInner.questions.map( (innerItems, i3) => { 
-              var key = "theQuestionName" + innerItems.id + "_" + itemInner.id;
-              
-              if(innerItems.safename){
-	              ret.push(
-	                <View key={key}>
-	                  <Text style={styles.header3}>
-	                    { this.state.showDebug ? (<Text style={styles.debugTexts}>#{key}#</Text>) : null }
-	                    {innerItems.safename}
-	                  </Text>  
-	                </View>
-	              );
-			  }          
+		                <Text style={styles.header2}>
+		                  { this.state.showDebug ? (<Text style={styles.debugTexts}>#{key}#</Text>) : null }
+		                  {itemInner.questionSafename}
+		                </Text>
+		              </View>
+		            );
+		        }
 
-              let ret2 = [];
-              innerItems.list.map( (selList, i4) => {
-                var key = "selList" + item.groupId + "_" + i4 + "_" + i3 + "_" + i2;
-                ret2.push(  
-                  <TouchableHighlight
-                    onPress={ (evt) => this.onPress(key,evt) } 
-                    key={key}>
-                      <View style={{...styles.selectableChoice,
-                      backgroundColor: this.state[key +'-active'] ? this.state.activeColor : this.state.inactiveColor,
-                      }}>
-                        <Text>
-                          { this.state.showDebug ? (<Text style={styles.debugTexts}>#{key}#</Text>) : null }
-                          {selList}
-                        </Text> 
-                      </View> 
-                  </TouchableHighlight>
-                );
-              });
+		        return itemInner.items.map( (innerItems, i3) => { 
+		          var key = "listsQuestionName|" + item.id + "_" + innerItems.id + "_" + itemInner.id;
+		          
+		          if(innerItems.safename){
+		              ret.push(
+		                <View key={key}>
+		                  <Text style={styles.header3}>
+		                    { this.state.showDebug ? (<Text style={styles.debugTexts}>#{key}#</Text>) : null }
+		                    {innerItems.safename}
+		                  </Text>  
+		                </View>
+		              );
+				  }
 
-              var key = "theQuestionChoices" + innerItems.id + "_" + itemInner.id;
-              ret.push(
-                <View key={key} style={styles.selectableChoiceParent}>
-                  {ret2} 
-                </View>
-                
-              );
+		          let ret2 = []; 
+		          let listOfChoices = innerItems.list.map( (selList, i4) => {
+		            var key = "selectListChoice|" + item.id + "_" + innerItems.id + "_" + itemInner.id + "_" + i4;
+		            ret2.push(  
+		              <TouchableHighlight
+			                onPress={  
+					    	(evt) => {
+					    		innerItems.selectedValue = i4 + 1; //HUOMAA TÄMÄ luonnissa!
+					    		this.choiceSelected(key,evt,listItems);	
+					    	}
+					    } 
+		                key={key}>
+		                  <View style={{...styles.selectableChoice,
+		                  backgroundColor: this.state[key +'-active'] ? this.state.activeColor : this.state.inactiveColor,
+		                  }}>
+		                    <Text>
+		                      { this.state.showDebug ? (<Text style={styles.debugTexts}>#{key}#</Text>) : null }
+		                      {selList}
+		                    </Text> 
+		                  </View> 
+		              </TouchableHighlight>
+		            );
+		            return selList;
+		          });
 
-            });
-          }   
-        }
-      });
+		          var key = "theQuestionChoices|" + item.id + "_" + innerItems.id + "_" + itemInner.id;
+		          ret.push(
+		            <View key={key} style={styles.selectableChoiceParent}>
+		              {ret2} 
+		            </View>
+		            
+		          );
+		          //return listOfChoices;
+		          return innerItems;
+		        });
+		      }   
+		    }
+		  });
 
-      var key = "masterView" + item.groupId;
-      return(
-        <View key={key}>
-          {ret}
-        </View>
-      ); 
+			
+		  var key = "masterView|" + item.id;
+		  ret.push(
+		    <View key={key}>
+		      {ret}
+		    </View>
+		  ); 
 
-    });
-    return listItems;
-  }
+		});
 
-  render() {
-    return (
-      <View>{this.createEl()}</View>
-    );
-  }
+		//console.log(listItems);
+		return ret;
+	}
+
+
+	render() {
+		return (
+			<View key="masterView">
+				<TouchableHighlight 
+					style={{...styles.button,
+		        		backgroundColor: this.state.showDebug ? this.state.activeColor : this.state.inactiveColor,
+		        	}} 
+		        	onPress={ (evt) => this.debugTexts() }>
+		        	<Text>
+		        		Debug {this.state.showDebug}
+		        	</Text>
+		        </TouchableHighlight>
+				<TouchableHighlight style={styles.button} onPress={(evt) => this.storeData(this.state.txt,"tieto1") }><Text>Test write</Text></TouchableHighlight>
+				<TouchableHighlight style={styles.button} onPress={(evt) => this.retrieveData("tieto1") }><Text>Test read</Text></TouchableHighlight>
+				{this.createEl()}
+			</View>
+		);
+	}
 }
+
 
 let perfectCheckboxSize = 50;
 let padding = 15;
@@ -300,6 +399,18 @@ const styles = StyleSheet.create({
     //borderWidth:1,
     //borderColor:'#d6d7da',
     marginRight:padding
+  },
+
+  //button
+  button: {
+    height:perfectChecbox,
+    alignItems:"center",
+    justifyContent:"center",
+    borderRadius:borderRadius,
+    //borderWidth:1,
+    //borderColor:'#d6d7da',
+    margin:padding,
+    backgroundColor: '#FFF',
   },
 });
 
